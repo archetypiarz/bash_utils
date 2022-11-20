ef1a728e97de96eefaa924cf9c45377d4b5560c2
archetypiarz <archetypiarz@gmail.com>
2022-11-20 20:03:18 +0100

    Added validation of passed arguments and files to scripts

--- addShebangLineToBegginingOfFile.sh
+++ addShebangLineToBegginingOfFile.sh
@@ -1,2 +1,27 @@
 #!/usr/bin/env bash
-sed -i '1s/^/#!\/usr\/bin\/env bash\n' file.sh
+
+# Had some troubles with using that escaped string both in echo and in sed. Finally decided to use two variables.
+BASH_SHEBANG_ESCAPED="#!\/usr\/bin\/env bash"
+BASH_SHEBANG="#!/usr/bin/env bash"
+
+if [[ $# -lt 1 ]]
+then
+	echo "No argument has been typed. Please enter a path leading to the valid file."
+elif [[ -f $1 ]]
+then
+	if ! [[ -s $1 ]]
+	then
+		echo "File is empty. Using echo instead of sed."
+		echo ${BASH_SHEBANG} > $1
+	else
+		FILE_FIRST_LINE=$(sed -n '1p' $1)
+		if [[ $FILE_FIRST_LINE == $BASH_SHEBANG ]]
+		then
+			echo "There is a shebang in this file already."
+		else
+			sed -i "1s/^/${BASH_SHEBANG_ESCAPED}\n/" $1
+		fi
+	fi
+else
+	echo "$1 isn't leading to a regular file. Please enter a proper path to a valid file."
+fi
--- copyStringOrLineFromFileToClipboard.sh
+++ copyStringOrLineFromFileToClipboard.sh
@@ -11,7 +11,7 @@ HELP_STRING="Options to choose:${NEWLINE}"\
 "${TABULATOR}-s / --string args...: copies args to clipboard.${NEWLINE}"\
 "${TABULATOR}-h / --help: shows help on how to use the script.${NEWLINE}"
 
-if [[ $# -lt 1 || $1 == "-h" || $1 == "--help" ]]
+if [[ $1 == "-h" || $1 == "--help" ]]
 then
 	echo "${NEWLINE}"\
 	"$HELP_HEADER"\
@@ -20,19 +20,25 @@ then
 	"$HELP_STRING"
 elif [[ $# -lt 2 ]]
 then
-	echo "You haven\'t entered any argument. Please enter proper arguments.${NEWLINE}"$HELP_STRING
+	echo "If you're not seeking for script help, you need to pass at least two arguments: an operation key and next args.${NEWLINE}$HELP_STRING"
 else
 	if [[ $1 == "-f" || $1 == "--file" ]]
 	then
-		sed -n '2p' $2 | xclip -selection clipboard
+		if [[ -f $2 ]]
+		then
+			sed -n '2p' $2 | xclip -selection clipboard
+		else
+			echo "$2 isn't leading to a regular file. Please enter a proper path to a valid file."
+		fi
 	elif [[ $1 == "-s" || $1 == "--string" ]]
 	then
-
 		STRING_TO_BE_COPIED=""
 		for argument in ${@:2}
 		do 
 			STRING_TO_BE_COPIED=$STRING_TO_BE_COPIED" "$argument
 		done
 		echo $STRING_TO_BE_COPIED | xclip -selection clipboard
+	else
+		echo "You haven't entered valid operation key. Please enter proper operation key with its arguments.${NEWLINE}$HELP_STRING"
 	fi
 fi
