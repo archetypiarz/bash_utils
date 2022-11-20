c44cdbaee3e0bc87b8dff7d7a5cf0143bb0e01d7
archetypiarz <archetypiarz@gmail.com>
2022-11-20 20:09:45 +0100

    Add two maven simple archetype scripts

---
+++ maven_utils/createBlankMavenProject.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mvn archetype:generate -DgroupId=pl.com.sysy -DartifactId=ModuleTrainingMaven -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart
